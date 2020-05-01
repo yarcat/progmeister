94be6a858c3fa58eb02a23842fb613482f9aadda
yarcat <yarcat@gmail.com>
2020-05-01 17:13:55 +0200

    Update doc.go

--- s01/slices/doc.go
+++ s01/slices/doc.go
@@ -1,4 +1,6 @@
 // Package slices contains test functions for https://progmeister.ch/blog/problems/slices/.
 //
 // Template application for these exercises are available at https://play.golang.org/p/ttE9fzLzhpU.
+//
+// To install locally: go get github.com/yarcat/progmeister/s01/slices
 package slices
