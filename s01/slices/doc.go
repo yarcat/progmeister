ef4ff423e600b4c56cc4dcc945739efe05a6a2b4
Jaroslavs Samcuks <yarcat@gmail.com>
2020-05-01 16:05:43 +0200

    play reference

--- s01/slices/doc.go
+++ s01/slices/doc.go
@@ -1,2 +1,4 @@
-// Package slices contains test functions for https://progmeister.ch/blog/problems/slices/
+// Package slices contains test functions for https://progmeister.ch/blog/problems/slices/.
+//
+// Template application for these exercises are available at https://play.golang.org/p/ttE9fzLzhpU.
 package slices
