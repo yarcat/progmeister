e68ddc193430d184483338051ca36ab8c242f3e4
Jaroslavs Samcuks <yarcat@gmail.com>
2020-06-19 14:56:24 +0200

    added colored hello world

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yarcat/progmeister
 
 go 1.14
+
+require github.com/gookit/color v1.2.5
---
+++ s01/terminal/hello/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/gookit/color"
+)
+
+func main() {
+	color.Cyan.Println("Hello, world!")
+
+	var colors []color.Color
+	for _, c := range color.FgColors {
+		colors = append(colors, c)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	for _, r := range "Hello, world!" {
+		i := rand.Int() % len(colors)
+		colors[i].Print(string(r))
+	}
+	color.Println()
+}
